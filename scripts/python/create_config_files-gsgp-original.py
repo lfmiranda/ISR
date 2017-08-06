a48785240c4a2897f0f1cc53e529b0b73af7882f
lfmiranda <luisfmir@gmail.com>
2017-08-06 13:23:50 -0300

    Update the script that creates configuration files for the GSGP experiments
    Changes:
    [update] Small changes in order to adapt the script for an experiment using the normalized datasets (id 123).
    [update] Code reorganization.

--- scripts/python/create_config_files-gsgp-original.py
+++ scripts/python/create_config_files-gsgp-original.py
@@ -1,39 +1,56 @@
 from os import chmod, makedirs, path
 
 # experiments variables
-systems = ["GSGP-Original"]
+system_id = "5-gsgp-03.07.2017"
+jar_name = "GSGP-Original.jar"
 datasets = ["airfoil", "ccn", "ccun", "concrete", "energyCooling", "energyHeating", "keijzer-6", "keijzer-7",
-            "parkinsons", "ppb", "towerData", "vladislavleva-1", "wineRed", "wineWhite", "yacht"]
-experiment_id = 1
+            "parkinsons", "ppb-wth0s", "towerData", "vladislavleva-1", "wineRed", "wineWhite", "yacht"]
+exper_number = 2
+exper_date = "06.08.2017"
+output_number = 123
 
-# paths related to the experiments
+# local paths
 root = path.expanduser("~") + "/Dropbox/my_files/research/ISR/"
-experiment_path = "experiments/5-gsgp-03.07.2017/"
+exper_local_path = root + "experiments/" + system_id + "/"
 
-server_root = "/home/speed/luisfmiranda/research/isr/"
-original_datasets_server_path = server_root + "datasets/original/"
-experiment_path_server = server_root + experiment_path
-output_path_server = server_root + "outputs/"
+# server paths
+server_root = "/home/fernando/research/ISR/"
+original_datasets_server_path = server_root + "datasets/normalized/"
+exper_server_path = server_root + "experiments/" + system_id + "/"
+output_server_path = server_root + "outputs/"
+
+# create and change the permition for the folder in which the output files will be written
+if not path.exists(exper_local_path + "outputs/"):
+    makedirs(exper_local_path + "outputs/")
+chmod(exper_local_path + "outputs/", 0o777)
+
+# change the permission for the jar file
+chmod(exper_local_path + jar_name, 0o777)
+
+# full identifier of the experiment
+exper_id = "-".join([str(exper_number), exper_date, "normalized"])
+
+# full identifier of the output folder
+output_folder = "-".join([str(output_number), system_id, exper_id]) + "/"
 
 # local folder in which the configuration files will be written
-config_files_folder = root + experiment_path + str(experiment_id) + "-config_files/"
+config_files_local_path = exper_local_path + exper_id + "/"
+if not path.exists(config_files_local_path):
+    makedirs(config_files_local_path)
 
-if not path.exists(config_files_folder):
-    makedirs(config_files_folder)
-
-print("Creating config files at: " + config_files_folder)
+print("Creating config files at: " + config_files_local_path)
 
 # batch script header
-batch_file = open(config_files_folder + "slurm_batch.sh", "w")
-chmod(config_files_folder + "slurm_batch.sh", 0o777)
+batch_file = open(config_files_local_path + "slurm_batch.sh", "w")
+chmod(config_files_local_path + "slurm_batch.sh", 0o777)
 batch_file.write("#!/bin/bash\n\n")
 batch_file.write("#SBATCH --nodelist <node>\n")
 batch_file.write("#SBATCH --exclusive\n\n")
 
 # generation of the configuration files
-parent_file = open(config_files_folder + "master.txt", "w")
+parent_file = open(config_files_local_path + "master.txt", "w")
 
-parent_file.write("experiment.output.dir = " + output_path_server + "\n")
+parent_file.write("experiment.output.dir = " + output_server_path + output_folder + "\n")
 parent_file.write("experiment.seed = 123456\n")
 parent_file.write("tree.build.terminals = edu.gsgp.nodes.terminals.ERC\n")
 parent_file.write("tree.build.functions = edu.gsgp.nodes.functions.AQ,edu.gsgp.nodes.functions.Sub,"
@@ -49,7 +66,7 @@ parent_file.write("evol.num.generation = 250\n")
 parent_file.write("experiment.num.repetition = 50\n")
 parent_file.write("pop.size = 1000\n")
 parent_file.write("rt.pool.size = 200\n")
-parent_file.write("evol.num.threads = 2\n")
+parent_file.write("evol.num.threads = 8\n")
 parent_file.write("pop.ind.selector.tourn.size = 10\n")
 parent_file.write("evol.min.error = 0\n")
 parent_file.write("breed.mut.step = 0.1\n")
@@ -63,15 +80,23 @@ parent_file.write("breed.spread.prob = 0.5\n")
 parent_file.write("breed.spread.alpha = 2.0\n")
 
 for dataset in datasets:
-    curr_child_file = open(config_files_folder + dataset + ".txt", "w")
+    # create the child configuration file
+    curr_child_file = open(config_files_local_path + dataset + ".txt", "w")
 
-    curr_child_file.write("parent = " + experiment_path_server + str(experiment_id) + "-config_files/master.txt\n")
+    curr_child_file.write("parent = " + exper_server_path + exper_id + "/master.txt\n")
     curr_child_file.write("experiment.data = " + original_datasets_server_path + dataset + "-train-#.csv\n")
     curr_child_file.write("experiment.data.test = " + original_datasets_server_path + dataset + "-test-#.csv\n")
-    curr_child_file.write("experiment.file.prefix = output-" + dataset + "\n")
+    curr_child_file.write("experiment.file.prefix = output-files/output-" + dataset + "\n")
 
-    batch_file.write("java -Xms512m -Xmx8g -jar " + "../GSGP-Original.jar -p " + experiment_path_server +
-                     str(experiment_id) + "-config_files/" + dataset + ".txt > " + output_path_server + dataset +
-                     ".txt\n")
+    # append the execution command to the batch file
+    batch_file.write("java -Xms512m -Xmx8g -jar " + exper_server_path + jar_name + " -p " + exper_server_path +
+                     exper_id + "/" + dataset + ".txt > " + output_server_path + output_folder + dataset + ".txt\n")
+
+# server folder in which the output files will be written
+output_local_path = exper_local_path + "outputs/" + output_folder
+if not path.exists(output_local_path + "output-files/"):
+    makedirs(output_local_path + "output-files/")
+chmod(output_local_path + "output-files/", 0o777)
+chmod(output_local_path, 0o777)
 
 print("Done.")
