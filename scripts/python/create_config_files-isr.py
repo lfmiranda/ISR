6b70244dd38b8910f200fa806d0fc3a9ee894e95
lfmiranda <luisfmir@gmail.com>
2017-09-02 14:18:57 -0300

    Update the script that creates parameter files for the weighting algorithm

--- scripts/python/create_config_files-isr.py
+++ scripts/python/create_config_files-isr.py
@@ -5,15 +5,23 @@ systems = ["ISR"]
 embeddings = ["original"]
 selection_level = "no_selection"
 schemes = ["proximity-xy", "surrounding-xy", "remoteness-xy"]
-dist_metrics = ["0.1", "0.5", "1.0", "2.0"]
-numbers_neighbors = ["4", "6", "9"]
+dist_metrics = ["1.0", "2.0"]
 dataset_type = "train"
-# The "ppb" dataset had to be modified in order to apply the embedding methods on it. That means that there are two
-# versions of the dataset ("ppb" and "ppb-wth0s"). Because the ISR performs a normalization step, the modified version
-# must be used.
 datasets = ["airfoil", "ccn", "ccun", "concrete", "energyCooling", "energyHeating", "keijzer-6", "keijzer-7",
-            "parkinsons", "ppb-wth0s", "towerData", "vladislavleva-1", "wineRed", "wineWhite", "yacht"]
-synthetic_datasets = ["keijzer-6", "keijzer-7", "vladislavleva-1"]
+            "parkinsons", "towerData", "vladislavleva-1", "wineRed", "wineWhite", "yacht"]
+
+# kna stands for k = number of attributes.
+# k1pni and k5pni stands for k = 1% and 5% of the number of instances, respectively.
+neighborhood_size_ids = ["kna", "k1", "k1pni", "k5pni"]
+
+# datasets information (necessary for defining, in some cases, the neighborhood sizes)
+numInst = {"airfoil": 1503, "ccn": 1994, "ccun": 1994, "concrete": 1030, "energyCooling": 768, "energyHeating": 768,
+           "keijzer-6": 50, "keijzer-7": 100, "parkinsons": 5875, "towerData": 4999, "vladislavleva-1": 100,
+           "wineRed": 1599, "wineWhite": 4898, "yacht": 308}
+
+numAttrs = {"airfoil": 5, "ccn": 122, "ccun": 124, "concrete": 8, "energyCooling": 8, "energyHeating": 8,
+            "keijzer-6": 2, "keijzer-7": 2, "parkinsons": 18, "towerData": 25, "vladislavleva-1": 3, "wineRed": 11,
+            "wineWhite": 11, "yacht": 6}
 
 # paths related to the experiments
 root = path.expanduser("~") + "/Dropbox/my_files/research/ISR/"
@@ -38,35 +46,33 @@ command_id = 1
 # generation of the configuration files
 for system in systems:
     for embedding in embeddings:
-        # set the input path
-        if embedding == "original":
-            input_path = original_datasets_path
-        else:
-            input_path = embeddings_path + embedding + "/"
+        input_path = original_datasets_path
 
         for scheme in schemes:
-            for number_neighbors in numbers_neighbors:
+            for neighborhood_size_id in neighborhood_size_ids:
                 for dist_metric in dist_metrics:
-                    # for the nonlinearity scheme only the Euclidean distance was used
-                    if scheme == "nonlinearity" and dist_metric != "2.0":
-                        continue
-
                     for dataset in datasets:
-                        # The embedding methods were not applied to synthetic datasets, thus no configuration file
-                        # should be created for this type of dataset.
-                        if (dataset in synthetic_datasets) and (embedding != "original"):
-                            continue
+                        assert neighborhood_size_id in ["kna", "k1", "k1pni", "k5pni"]
 
-                        # The nonlinearity scheme could not be applied to the ppb dataset, since it has more attributes
-                        # than instances.
-                        if (scheme == "nonlinearity") and (dataset == "ppb-wth0s"):
-                            continue
+                        # find the actual number of neighbors for each dataset
+                        if neighborhood_size_id == "kna":
+                            num_neighbors = numAttrs[dataset]
+                        elif neighborhood_size_id == "k1pni":
+                            num_neighbors = int(0.01 * numInst[dataset])
+                        elif neighborhood_size_id == "k5pni":
+                            num_neighbors = int(0.05 * numInst[dataset])
+                        else:
+                            num_neighbors = 1
 
-                        experiment_name = "-".join([embedding, scheme, "k" + number_neighbors, "L" + dist_metric,
-                                                    dataset])
+                        if num_neighbors < 1:
+                            num_neighbors = 1
+                        num_neighbors = str(num_neighbors)
+
+                        experiment_name = "-".join([embedding, scheme, "k" + num_neighbors, "L" + dist_metric, dataset])
 
                         parameter_file = open(config_files_folder + experiment_name + ".txt", "w")
 
+                        # parameters
                         parameter_file.write("original.folds.path = " + original_datasets_path + "\n")
                         parameter_file.write("input.path = " + input_path + "\n")
                         parameter_file.write("output.path = " + output_path + embedding + "/\n")
@@ -74,7 +80,7 @@ for system in systems:
                         parameter_file.write("scheme = " + scheme + "\n")
                         parameter_file.write("selection.level = " + selection_level + "\n")
                         parameter_file.write("distance.metric = " + dist_metric + "\n")
-                        parameter_file.write("number.neighbors = " + number_neighbors + "\n")
+                        parameter_file.write("number.neighbors = " + num_neighbors + "\n")
 
                         batch_file.write("echo \"Current file: " + str(command_id) + " - " + experiment_name + "\"\n")
                         batch_file.write("java -ea -jar " + experiments_path + "ISR.jar -p " + experiments_path +
