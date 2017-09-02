d7273c8139f511bd0cbdab296ffdad74e19f7a0d
lfmiranda <luisfmir@gmail.com>
2017-09-02 12:25:08 -0300

    Update the script that plots the instance weights

--- scripts/python/plot_weights-2d.py
+++ scripts/python/plot_weights-2d.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 import os
 
 # experiments variables
-datasets = ["ksinc-0"]
-schemes = ["proximity-x", "proximity-xy", "surrounding-x", "surrounding-xy", "remoteness-x", "remoteness-xy"]
-dist_metrics = ["L0.1", "L0.5", "L1.0", "L2.0"]
+datasets = ["salustowicz1d-train-0"]
+schemes = ["proximity-xy", "surrounding-xy", "remoteness-xy"]
+dist_metrics = ["L2.0"]
+nums_neighbors = ["k2", "k5"]
 
 # local paths
 base_dir = os.path.expanduser("~/Dropbox/my_files/research/ISR/")
 data_folder = base_dir + "datasets/original/"
 weights_folder = base_dir + "datasets/2-08.06.2017/original/weights/"
-plots_folder = base_dir + "plots/3-22.07.2017/original/"
+plots_folder = base_dir + "plots/5-01.09.2017/original/"
 
 if not os.path.exists(plots_folder):
     os.makedirs(plots_folder)
@@ -25,45 +26,52 @@ for dataset in datasets:
     for scheme in schemes:
         print("  Scheme: " + scheme)
 
-        for dist_metric in dist_metrics:
-            weights = weights_folder + "-".join([scheme, dist_metric, dataset]) + ".csv"
+        for num_neighbors in nums_neighbors:
+            if scheme == "nonlinearity":
+                nonlinearitySchemeAlreadyExecuted = False
 
-            print("    Distance metric: " + dist_metric)
-            print("    Data file: " + fold)
-            print("    Weight file: " + weights)
+            for dist_metric in dist_metrics:
+                weights = weights_folder + "-".join([scheme, num_neighbors, dist_metric, dataset]) + ".csv"
 
-            # read and process the datasets
-            data = np.loadtxt(fold, delimiter=",")
-            X = data[:, 0]
-            Y = data[:, 1]
+                print("    Distance metric: " + dist_metric)
+                print("    Data file: " + fold)
+                print("    Weight file: " + weights)
 
-            # read and process the wheight files
-            W = np.loadtxt(weights, delimiter=",")
-            labels = ['%.2f' % elem for elem in W.tolist()]
-            min_weight = min(W) * 0.95
-            max_weight = max(W) * 0.95
+                # read and process the datasets
+                data = np.loadtxt(fold, delimiter=",")
+                X = data[:, 0]
+                Y = data[:, 1]
 
-            areas = [(w - min_weight + 0.05) / (max_weight - min_weight) * 400 for w in W]
+                # read and process weight files
+                W = np.loadtxt(weights, delimiter=",")
+                labels = ['%.2f' % elem for elem in W.tolist()]
+                min_weight = min(W) * 0.95
+                max_weight = max(W) * 0.95
 
-            plt.figure(figsize=(16, 8))
+                areas = [(w - min_weight) / (max_weight - min_weight) * 400 for w in W]
 
-            ax = plt.subplot(111)
-            ax.spines["top"].set_visible(False)
-            ax.spines["right"].set_visible(False)
+                plt.figure(figsize=(16, 8))
 
-            plt.scatter(X, Y, s=areas, label="1", c="#1F77B4", facecolors="none", alpha=0.4)
+                ax = plt.subplot(111)
+                ax.spines["top"].set_visible(False)
+                ax.spines["right"].set_visible(False)
 
-            # plot the wheights of each instance
-            for label, x, y, area in zip(labels, data[:, 0], data[:, 1], areas):
-                plt.annotate(
-                    str(label).replace("0.", "."),
-                    color="#1F77B4",
-                    xy=(x, y), xytext=(0, 5 + math.sqrt(area)),
-                    textcoords="offset points", ha="center", va="center", fontsize=6,
-                    arrowprops=dict(arrowstyle='-', color="#1F77B4", connectionstyle='arc3,rad=0'))
+                plt.scatter(X, Y, s=areas, label="1", c="#1F77B4", facecolors="none", alpha=0.4)
 
-            plt.title("Weights - " + scheme + ", " + dist_metric + ", " + dataset)
-            plt.xlabel("Input", fontsize=12)
-            plt.ylabel("Output", fontsize=12)
-            plt.savefig(plots_folder + dataset + "-" + scheme + "-" + dist_metric + ".pdf", bbox_inches="tight")
-            plt.close()
+                # plot the weights of each instance
+                for label, x, y, area in zip(labels, data[:, 0], data[:, 1], areas):
+                    plt.annotate(
+                        str(label).replace("0.", "."),
+                        color="#1F77B4",
+                        xy=(x, y), xytext=(0, 5 + math.sqrt(area)),
+                        textcoords="offset points", ha="center", va="center", fontsize=6,
+                        arrowprops=dict(arrowstyle='-', color="#1F77B4", connectionstyle='arc3,rad=0'))
+
+                plt.xlabel("Input", fontsize=12)
+                plt.ylabel("Output", fontsize=12)
+
+                plt.title("Weights - " + scheme + ", " + dist_metric + ", " + dataset)
+                plt.savefig(plots_folder + "-".join([dataset, scheme, num_neighbors, dist_metric]) + ".pdf",
+                            bbox_inches="tight")
+
+                plt.close()
