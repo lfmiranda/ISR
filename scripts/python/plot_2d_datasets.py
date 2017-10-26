f2044e97a0c67719953f322ef87357f2423d3600
lfmiranda <luisfmir@gmail.com>
2017-10-26 19:51:46 -0200

    Add scripts to plots original datasets

---
+++ scripts/python/plot_2d_datasets.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+datasets = [
+    "f7-train-0",
+]
+
+# local paths
+base_dir = os.path.expanduser("~/Dropbox/my_files/research/ISR/")
+data_folder = base_dir + "datasets/original/"
+plots_folder = base_dir + "plots/14-17.10.2017/"
+
+if not os.path.exists(plots_folder):
+    os.makedirs(plots_folder)
+
+for dataset in datasets:
+    print("Dataset: " + dataset)
+
+    fold = data_folder + dataset + ".csv"
+
+    # read and process the datasets
+    data = np.loadtxt(fold, delimiter=",")
+    X = data[:, 0]
+    Y = data[:, 1]
+
+    plt.figure(figsize=(18, 10))
+
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+
+    ax = plt.subplot(111)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+
+    plt.tick_params(axis="both", which="both", bottom="on", top="off", left="on", right="off", labelbottom="on",
+                    labelleft="on")
+
+    X_max = max(X)
+    X_min = min(X)
+    Y_max = max(Y)
+    Y_min = min(Y)
+
+    X_range = X_max - X_min
+    Y_range = Y_max - Y_min
+
+    X_step = X_range / 4
+    Y_step = Y_range / 4
+
+    # customise the size of axis ticks
+    plt.xticks(np.arange(X_min, X_max + X_step, X_step),
+               [str(r"%.1f" % x) for x in np.arange(X_min, X_max + X_step, X_step)],
+               fontsize=20)
+
+    plt.yticks(np.arange(Y_min, Y_max + Y_step, Y_step),
+               [str(r"%.1f" % y) for y in np.arange(Y_min, Y_max + Y_step, Y_step)],
+               fontsize=20)
+
+    #plt.scatter(X, Y, s=70, facecolors="none", edgecolors="black")
+    plt.scatter(X, Y, s=50, c="#1F77B4", facecolors="none", alpha=0.4)
+
+    plt.xlabel(r"$input$", fontsize=20)
+    plt.ylabel(r"$output$", fontsize=20)
+
+    plt.savefig(plots_folder + dataset + ".pdf", bbox_inches="tight")
+    plt.close()
---
+++ scripts/python/plot_3d_datasets.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pylab
+
+from mpl_toolkits.mplot3d import Axes3D
+
+datasets = [
+    "kotanchek-test-0",
+]
+
+# local paths
+base_dir = os.path.expanduser("~/Dropbox/my_files/research/ISR/")
+data_folder = base_dir + "datasets/original/"
+plots_folder = base_dir + "plots/14-17.10.2017/"
+
+if not os.path.exists(plots_folder):
+    os.makedirs(plots_folder)
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+for dataset in datasets:
+    print("Dataset: " + dataset)
+
+    fold = data_folder + dataset + ".csv"
+
+    # read and process the datasets
+    data = np.loadtxt(fold, delimiter=",")
+    X = data[:, 0]
+    Y = data[:, 1]
+    Z = data[:, 2]
+
+    fig = pylab.figure(figsize=(16, 10))
+
+    ax = Axes3D(fig)
+
+    # get rid of the panes
+    ax.w_xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.w_yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.w_zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+
+    # get rid of the grid lines
+    ax.grid(False)
+
+    # customise the size of axis ticks
+    plt.xticks(range(0, 6, 1), [str(r"%d" % x) for x in range(0, 6, 1)], fontsize=14)
+    plt.yticks(range(0, 6, 1), [str(r"%d" % y) for y in range(0, 6, 1)], fontsize=14)
+    ax.zaxis.set_ticklabels(np.arange(0, 1, 0.2), [str(r"%.1f" % z) for z in np.arange(0, 1, 0.2)], fontsize=14)
+
+    ax.scatter(X, Y, Z, c="#1F77B4", facecolors="none", alpha=0.4)
+
+    ax.set_xlabel(r"$x1$", fontsize=20)
+    ax.set_ylabel(r"$x2$", fontsize=20)
+    ax.set_zlabel(r"$output$", fontsize=20)
+
+    plt.savefig(plots_folder + dataset + ".pdf", bbox_inches="tight")
+    plt.close()
